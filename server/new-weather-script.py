17d6e978b112329559d44a349f866d7f11c624ae
obitoo <avocalodge@gmail.com>
2014-04-04 19:07:14 +0100

    renamed template

--- server/new-weather-script.py
+++ server/new-weather-script.py
@@ -25,8 +25,7 @@ myApiKey="7557844e-c57a-4fc6-90d0-055fcce3018c"
 #
 #  magic Svg template
 #
-template = 'weather-script-preprocess_2.svg'
-template = 'weather-script-preprocess_3.svg'
+template = 'weather-script-preprocess_temps.svg'
 
 #
 #  Map the MetOffice weather codes to Icons. 
@@ -125,13 +124,24 @@ for period in periods:
 # Open SVG to process
 output = codecs.open(template , 'r', encoding='utf-8').read()
 
-# Insert icons and temperatures
-output = output.replace('ICON_ONE',icons[0]).replace('ICON_TWO',icons[1]).replace('ICON_THREE',icons[2]).replace('ICON_FOUR',icons[3])
-output = output.replace('HIGH_ONE',str(highs[0])).replace('HIGH_TWO',str(highs[1])).replace('HIGH_THREE',str(highs[2])).replace('HIGH_FOUR',str(highs[3]))
+# Insert weather icons and temperatures
+output = output.replace('ICON_ONE',icons[0])
+output = output.replace('ICON_TWO',icons[1])
+output = output.replace('ICON_THREE',icons[2])
+output = output.replace('ICON_FOUR',icons[3])
 
-output = output.replace('LOW_ONE',str(feels[0])).replace('LOW_TWO',str(feels[1])).replace('LOW_THREE',str(feels[2])).replace('LOW_FOUR',str(feels[3]))
+output = output.replace('HIGH_ONE',str(highs[0]))
+output = output.replace('HIGH_TWO',str(highs[1]))
+output = output.replace('HIGH_THREE',str(highs[2]))
+output = output.replace('HIGH_FOUR',str(highs[3]))
 
-# Insert current time (thanks Jennifer http://www.shatteredhaven.com/2012/11/1347365-kindle-weather-display.html)
+output = output.replace('LOW_ONE',str(feels[0]))
+output = output.replace('LOW_TWO',str(feels[1]))
+output = output.replace('LOW_THREE',str(feels[2]))
+output = output.replace('LOW_FOUR',str(feels[3]))
+
+# Insert current time 
+# (thanks Jennifer http://www.shatteredhaven.com/2012/11/1347365-kindle-weather-display.html)
 output = output.replace('DATE_VALPLACE',str(dtnow))
 
 # Insert days of week
@@ -142,7 +152,8 @@ days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturda
 print days_of_week[(today_dt + 2*one_day).weekday()]
 print days_of_week[(today_dt + 3*one_day).weekday()]
 
-output = output.replace('DAY_THREE',days_of_week[(today_dt + 2*one_day).weekday()]).replace('DAY_FOUR',days_of_week[(today_dt + 3*one_day).weekday()])
+output = output.replace('DAY_THREE',days_of_week[(today_dt + 2*one_day).weekday()])
+output = output.replace('DAY_FOUR',days_of_week[(today_dt + 3*one_day).weekday()])
 
 # Write output
 codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
