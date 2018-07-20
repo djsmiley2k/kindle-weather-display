5833a78d456dc0ee44295f8bb1c73a4aeb2aa0f9
Tim Bowers <djsmiley2k@gmail.com>
2018-07-20 12:47:32 +0100

    Added white background otherwise images are invisible on Kindle 3.

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -9,10 +9,10 @@ echo "------ python get data from metoffice"
 python2 new-weather-script.py
 
 echo "------ convert to png"
-convert -depth 8 weather-script-output.svg weather-script-output.png
+convert -depth 8 -background white weather-script-output.svg weather-script-output.png
 
 echo "------ shrink png"
-pngcrush -q -c 0 weather-script-output.png weather-script-output_s.png
+pngcrush -q -c 2 weather-script-output.png weather-script-output_s.png
 
 echo "------ copy to webserver"
 cp -f weather-script-output_s.png /var/www/localhost/htdocs/weather/weather-script-output.png
