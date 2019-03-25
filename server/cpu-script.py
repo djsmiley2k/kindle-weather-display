ecfd116463f2f852c9dcf284cd6db8f9ae9ba6c1
Tim Bowers <djsmiley2k@gmail.com>
2019-03-25 19:56:10 +0000

    Replace CPU Placeholders with values

--- server/cpu-script.py
+++ server/cpu-script.py
@@ -14,17 +14,17 @@ template = 'cpu-script-preprocess.svg'
 # Open SVG to process
 output = codecs.open(template , 'r', encoding='utf-8').read()
 
-# Insert values into SVG
-output = output.replace('ICON_ONE',icons[0])
-output = output.replace('ICON_TWO',icons[1])
-output = output.replace('ICON_THREE',icons[2])
-output = output.replace('ICON_FOUR',icons[3])
+output = output.replace('CPU_V',str(cpu_temp))
+output = output.replace('NB_V',str(nb_temp))
+output = output.replace('HDD_V',str(hdd_temp))
+output = output.replace('LOAD_V',str(load))
+output = output.replace('UPTIME_V',str(uptime))
 
 # Insert current time
 # (thanks Jennifer http://www.shatteredhaven.com/2012/11/1347365-kindle-weather-display.html)
 output = output.replace('DATE_VALPLACE',str(dtnow))
 
 # Write output
-codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
+codecs.open('cpu-script-output.svg', 'w', encoding='utf-8').write(output)
 
 # EOF
